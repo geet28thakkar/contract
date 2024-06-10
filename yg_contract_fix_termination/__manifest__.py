0423159a2b58785cc7f93bf06a09d10737d5f067
Yves Goldberg <admin@ygol.com>
2024-06-10 09:49:30 +0300

    refactor(yg_contract_fix_termination

---
+++ yg_contract_fix_termination/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2024 Yves Goldberg (Ygol InternetWork)
+# Part of module yg_contract_fix_termination. See LICENSE file for
+# full copyright and licensing details.
+
+{
+    'name': 'Yg Contract Fix Termination',
+    'description': """
+        Fix termination bug""",
+    'version': '16.0.1.0.0',
+    'license': 'Other proprietary',
+    'author': 'Yves Goldberg (Ygol InternetWork)',
+    'website': 'https://www.ygol.com',
+    'depends': [
+    ],
+    'external_dependencies': {"python": [], "bin": []},
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+    'uninstall_hook': None,
+
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
