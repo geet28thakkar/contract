e74607787017d708c7cac8c122186e3f1fae614d
Geet <geet@ygol.com>
2024-06-21 19:53:38 +0530

    [ADD] Added module which will fix the company restrictions in the contract module

---
+++ yg_conrtact_company_restriction/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Domatix - Carlos Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Contract company restriction",
+    "summary": "Contract company restriction.",
+    "version": "16.0.1.0.0",
+    "category": "Contract Management",
+    "license": "AGPL-3",
+    "depends": ["contract"],
+    "data": [
+    ],
+    "installable": True,
+    "application": True,
+}
